5ed97ec2102bd8fed214780c3e00ce753e82312b
ALEX <35903333+AL3XPI@users.noreply.github.com>
2019-11-22 19:31:37 -0500

    test

--- src/AL3XPI/KronosLobbyCORE.php
+++ src/AL3XPI/KronosLobbyCORE.php
@@ -29,8 +29,13 @@ class KronosLobbyCORE extends PluginBase implements Listener{
                     $this->getLogger()->info("KronosLobbyCORE by AL3XPI disabled");
           }
 	  public function onJoin(PlayerJoinEvent $event){
-  		 $player = $event->getPlayer();
+  		$player = $event->getPlayer();
    		$name = $player->getName();
    		$this->getServer()->broadcastMessage(C::GREEN."[+]$name");
-	}
+	 public function onItemHeld(PlayerItemHeldEvent $event){
+		if($item->getId() === Item::STICK){
+		$command = '1vs1 join' // You can get this command from config or whatever you want
+		$this->getServer()->dispatchCommand($event->getPlayer(), $command); // Dispatch the command;
+	 }
+}
 }
